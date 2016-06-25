657c26a7c8e8f9b957faa419e7bd2892eb6ca344
Thangiee <Thangiee0@gmail.com>
2016-06-25 10:25:46 -0500

    Add Session model

---
+++ src/main/scala/lolchat/model/Session.scala
@@ -0,0 +1,17 @@
+package lolchat.model
+
+import lolchat.util.EventStream
+import io.dylemma.frp.{EventSource, Observer}
+
+case class Session(user: String, passwd: String, region: Region, acceptFriendRequest: Boolean = false) {
+  private[lolchat] val obs = new Observer {}
+
+  private[lolchat] val msgEventSrc = EventSource[Msg]()
+  val msgStream: EventStream[Msg] = EventStream(this, msgEventSrc)
+
+  private[lolchat] val friendListEventSrc = EventSource[FriendListEvent]()
+  val friendListStream: EventStream[FriendListEvent] = EventStream(this, friendListEventSrc)
+
+  private[lolchat] val connectionEventSrc = EventSource[ConnectionEvent]()
+  val connectionEventStream: EventStream[ConnectionEvent] = EventStream(this, connectionEventSrc)
+}
