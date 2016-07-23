a12fb7d15ce964efe470209f2e149ab4158478eb
Thangiee <Thangiee0@gmail.com>
2016-07-23 12:24:38 -0500

    add abbr to Region

--- core/src/main/scala/lolchat/data/Region.scala
+++ core/src/main/scala/lolchat/data/Region.scala
@@ -1,16 +1,16 @@
 package lolchat.data
 
-case class Region(url: String) extends AnyVal
+case class Region(url: String, abbr: String)
 
 object Region {
-  val NA = Region("chat.na2.lol.riotgames.com")
-  val BR = Region("chat.br.lol.riotgames.com")
-  val EUNE = Region("chat.eun1.lol.riotgames.com")
-  val EUW = Region("chat.euw1.lol.riotgames.com")
-  val KR = Region("chat.kr.lol.riotgames.com")
-  val LAN = Region("chat.la1.lol.riotgames.com")
-  val LAS = Region("chat.la2.lol.riotgames.com")
-  val OCE = Region("chat.oc1.lol.riotgames.com")
-  val RU = Region("chat.ru.lol.riotgames.com")
-  val TR = Region("chat.tr.lol.riotgames.com")
+  val NA = Region("chat.na2.lol.riotgames.com", "na")
+  val BR = Region("chat.br.lol.riotgames.com", "br")
+  val EUNE = Region("chat.eun1.lol.riotgames.com", "eune")
+  val EUW = Region("chat.euw1.lol.riotgames.com", "euw")
+  val KR = Region("chat.kr.lol.riotgames.com", "kr")
+  val LAN = Region("chat.la1.lol.riotgames.com", "lan")
+  val LAS = Region("chat.la2.lol.riotgames.com", "las")
+  val OCE = Region("chat.oc1.lol.riotgames.com", "oce")
+  val RU = Region("chat.ru.lol.riotgames.com", "ru")
+  val TR = Region("chat.tr.lol.riotgames.com", "tr")
 }
